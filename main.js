52d28d70cc66bea222dbec6c5b9fb2e16ce5e0d5
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-11 17:39:04 +0200

    can t change img src

---
+++ main.js
@@ -0,0 +1,20 @@
+let changeImg = document.getElementsByTagName("img");
+changeImg.src = "http://boolean.careers/images/misc/logo.png";
+
+/* console.log(changeImg)  */
+
+const hamburgerBtn = document.getElementsByClassName("fas fa-bars")[0];
+const hamburgerMenu = document.getElementsByClassName("hamburger-menu")[0];
+const closeHamburgerMenu = document.getElementsByClassName("fas fa-times")[0];
+/* console.log(hamburgerMenu) */
+/* console.log(closeHamburgerMenu) */
+
+hamburgerBtn.addEventListener("click", function(){
+
+    hamburgerMenu.style.display = "block";
+});
+
+closeHamburgerMenu.addEventListener("click", function(){
+
+    hamburgerMenu.style.display = "none";
+})
